ed8dc68193a3427d23d0f00bf7e18261d0725b86
pybin94 <73331322+pybin94@users.noreply.github.com>
2022-05-09 18:16:15 +0900

    Add files via upload

--- script3.js
+++ script3.js
@@ -2,21 +2,22 @@ let level = 0;
 let count = 0;
 let profit = 0;
 let startAmountValue = 0;
-let reset = 9
+let reset = 22
+let multiWin = false;
 
 let martin = []
-const martin1 = [1000, 3000, 8000, 18000, 40000, 85000, 175000, 375000, 800000, 1600000];
-const martin2 = [2000, 7000, 17000, 40000, 85000, 175000, 375000, 775000, 1600000, 3000000];
-const martin3 = [3000, 10000, 25000, 55000, 125000, 250000, 525000, 1075000, 2200000, 3000000];
+const martin1 = [1000, 3000, 8000, 18000, 40000, 32000, 64000, 43000, 86000, 60000, 120000, 83000, 166000, 115000, 230000, 157000, 314000, 215000, 430000, 300000, 600000, 420000, 840000];
+const martin2 = [2000, 7000, 17000, 40000, 85000, 63000, 126000, 87000, 174000, 120000, 240000, 165000, 330000, 225000, 450000, 310000, 620000, 420000, 840000, 570000, 1140000, 780000, 1560000];
+const martin3 = [3000, 10000, 25000, 55000, 125000, 95000, 190000, 135000, 270000, 185000, 370000, 255000, 510000, 350000, 700000, 475000, 950000, 650000, 1300000, 880000, 1760000, 1200000, 2400000];
 
 let markAmount = []
 const markAmount1 = ["1000", "3000", "8000", "1.8k", "4k", "8.5k", "17.5k", "37.5k", "80k", "160k"];
 const markAmount2 = ["2000", "7000", "1.7k", "4k", "8.5k", "17.5k", "37.5k", "77.5k", "160k", "300k"];
 const markAmount3 = ["3000", "1k", "2.5k", "5.5k", "12.5k", "25k", "52.5k", "107.5", "220k", "300k"];
 
-const gamePattern = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+const gamePattern = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
 
-gamePattern[0] = "";
+// gamePattern[0] = 0;
 
 const init = () => {
     newGame()
@@ -115,24 +116,47 @@ const newGame = () => {
 
 const winGame = () => {
 
-    winProfit()
-    level = 0
-    randomLogic()
-    countRound()
-    levelAlert()
+    if(level < 5) {
+        winProfit()
+        level = 0
+        randomLogic()
+        countRound()
+        levelAlert()
+    } else if (level >= 5 && multiWin == false){
+        winProfit()
+        level++
+        multiWin = true;
+        randomLogic()
+        countRound()
+        levelAlert()
+        console.log(multiWin)
+
+    } else if (level >= 5 && multiWin == true) {
+        winProfit()
+        multiWin = false
+        level = 0
+        randomLogic()
+        countRound()
+        levelAlert()
+    }
+
 }
 
 const loseGame = () => {
 
     loseProfit()
-
     randomLogic()
+
+    let multiWin = false;
+
     if (level == reset){
         alert("리셋 단계에 도달했습니다 게임을 다시 시작합니다.")
         return logicReset()
     }
-
     level++
+
+    console.log(level)
+    console.log(reset)
 
     displayScreen()
     countRound()
@@ -187,9 +211,9 @@ const setOK = () => {
 
     let markList = ""
 
-    markAmount[0].map((item, index) => {
-        markList += `<div class="markAmount">${item}</div>`
-    })
+    // markAmount[0].map((item, index) => {
+    //     markList += `<div class="markAmount">${item}</div>`
+    // })
     
     newGame()
     displayScreen()
